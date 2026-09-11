fc04c6b1a2d8c75299db2361f80f25152dc03e89
waxew <4622.ar@gmail.com>
2026-09-11 23:29:47 +0330

    Add initial user profile screen

---
+++ app/src/main/java/com/asteam/itaghvim/presentation/profile/ProfileScreen.kt
@@ -0,0 +1,13 @@
+package com.asteam.itaghvim.presentation.profile
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+/**
+ * صفحه پروفایل کاربر
+ * مدیریت تصویر، اطلاعات کاربر و وضعیت VIP در این بخش قرار می گیرد.
+ */
+@Composable
+fun ProfileScreen() {
+    Text(text = "پروفایل کاربر")
+}

bce41a63087574fcd5de9422cb5b6452cf7901fd
waxew <4622.ar@gmail.com>
2026-09-11 23:26:45 +0330

    Add reusable application header component

---
+++ app/src/main/java/com/asteam/itaghvim/presentation/components/AppHeader.kt
@@ -0,0 +1,13 @@
+package com.asteam.itaghvim.presentation.components
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+/**
+ * سربرگ مشترک برنامه
+ * در مراحل بعدی شامل همبرگری، لوگو و منوی سمت چپ خواهد شد.
+ */
+@Composable
+fun AppHeader(){
+    Text(text = "آی تقویم")
+}

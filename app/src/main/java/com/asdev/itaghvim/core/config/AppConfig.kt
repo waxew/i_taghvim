9ba8b1df688b30629980d285cbfad41a9c9d40b7
waxew <4622.ar@gmail.com>
2026-09-11 23:51:40 +0330

    Add application config base

---
+++ app/src/main/java/com/asdev/itaghvim/core/config/AppConfig.kt
@@ -0,0 +1,12 @@
+package com.asdev.itaghvim.core.config
+
+/**
+ * تنظیمات پایه برنامه آی تقویم
+ *
+ * اطلاعات عمومی برنامه در این فایل متمرکز می‌شود
+ * تا در بخش‌های مختلف پروژه دوباره نویسی نشود.
+ */
+object AppConfig {
+    const val APP_NAME = "آی تقویم"
+    const val PACKAGE_NAME = "com.asdev.itaghvim"
+}

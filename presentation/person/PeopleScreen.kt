d28c90f701fec1ca5e8fab4ec3fe589cb487c2fc
waxew <4622.ar@gmail.com>
2026-09-11 23:43:51 +0330

    Add initial PeopleScreen UI

---
+++ presentation/person/PeopleScreen.kt
@@ -0,0 +1,11 @@
+package com.asteam.itaghvim.presentation.person
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+// صفحه نمایش لیست اشخاص
+@Composable
+fun PeopleScreen() {
+    // UI کامل در مرحله بعد با LazyColumn و Search اضافه می‌شود
+    Text(text = "لیست اشخاص")
+}

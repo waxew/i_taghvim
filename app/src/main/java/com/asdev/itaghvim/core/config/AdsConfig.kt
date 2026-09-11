788de5c3790c2945218b882a0a06a81312a19639
waxew <4622.ar@gmail.com>
2026-09-11 23:51:45 +0330

    Add ads configuration

---
+++ app/src/main/java/com/asdev/itaghvim/core/config/AdsConfig.kt
@@ -0,0 +1,12 @@
+package com.asdev.itaghvim.core.config
+
+/**
+ * تنظیمات تبلیغات برنامه
+ *
+ * فعال یا غیرفعال بودن تبلیغات از این نقطه کنترل می‌شود.
+ */
+object AdsConfig {
+    const val ADS_ENABLED = false
+    const val SHOW_FOR_GUEST_USERS = true
+    const val HIDE_FOR_VIP_USERS = true
+}

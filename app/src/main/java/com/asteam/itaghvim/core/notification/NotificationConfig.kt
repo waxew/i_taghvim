47c2e53d62c129e813cc42b38a0b2d6320d41225
waxew <4622.ar@gmail.com>
2026-09-11 23:29:50 +0330

    Add notification configuration

---
+++ app/src/main/java/com/asteam/itaghvim/core/notification/NotificationConfig.kt
@@ -0,0 +1,13 @@
+package com.asteam.itaghvim.core.notification
+
+/**
+ * تنظیمات اعلان های داخلی برنامه
+ */
+object NotificationConfig {
+
+    // فعال سازی اعلان های برنامه
+    const val ENABLED = true
+
+    // نمایش اعلان بررسی نسخه جدید
+    const val SHOW_UPDATE_NOTIFICATION = true
+}

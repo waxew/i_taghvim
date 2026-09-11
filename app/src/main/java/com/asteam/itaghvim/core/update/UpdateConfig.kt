9d0f3265e430d77682e135383537f81fc58555aa
waxew <4622.ar@gmail.com>
2026-09-11 23:29:17 +0330

    Add update configuration foundation

---
+++ app/src/main/java/com/asteam/itaghvim/core/update/UpdateConfig.kt
@@ -0,0 +1,9 @@
+package com.asteam.itaghvim.core.update
+
+/**
+ * تنظیمات بررسی بروزرسانی برنامه
+ */
+object UpdateConfig {
+    const val ENABLE_AUTO_CHECK = true
+    const val UPDATE_JSON_URL = ""
+}

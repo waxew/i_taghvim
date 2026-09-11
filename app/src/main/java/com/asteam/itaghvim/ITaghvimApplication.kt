da14e455ce376bb7b9c8a15fe0674d99b6a2df97
waxew <4622.ar@gmail.com>
2026-09-11 23:27:04 +0330

    Add Hilt application class

---
+++ app/src/main/java/com/asteam/itaghvim/ITaghvimApplication.kt
@@ -0,0 +1,11 @@
+package com.asteam.itaghvim
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * کلاس اصلی برنامه
+ * نقطه شروع Dependency Injection با Hilt
+ */
+@HiltAndroidApp
+class ITaghvimApplication : Application()

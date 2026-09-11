18fcfdb12d4204f2f159aa01653fa1c4c6a59e77
waxew <4622.ar@gmail.com>
2026-09-11 23:57:28 +0330

    Add application DI foundation

---
+++ app/src/main/java/com/asdev/itaghvim/core/di/AppModule.kt
@@ -0,0 +1,9 @@
+package com.asdev.itaghvim.core.di
+
+/**
+ * ماژول عمومی Dependency Injection
+ * محل قرارگیری وابستگی های عمومی برنامه
+ */
+object AppModule {
+    // آماده برای اضافه شدن binding های عمومی
+}

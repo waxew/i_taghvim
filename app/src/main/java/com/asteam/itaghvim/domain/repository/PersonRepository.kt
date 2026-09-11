585c1e853f326430ba1f7dbadef5c0625eb9c9bf
waxew <4622.ar@gmail.com>
2026-09-11 23:25:33 +0330

    Add domain repository contract

---
+++ app/src/main/java/com/asteam/itaghvim/domain/repository/PersonRepository.kt
@@ -0,0 +1,15 @@
+package com.asteam.itaghvim.domain.repository
+
+import com.asteam.itaghvim.domain.model.Person
+import kotlinx.coroutines.flow.Flow
+
+/**
+ * قرارداد دسترسی به اطلاعات اشخاص
+ * لایه Domain نباید وابسته به دیتابیس باشد.
+ */
+interface PersonRepository {
+    fun getPersons(): Flow<List<Person>>
+    suspend fun insertPerson(person: Person)
+    suspend fun deletePerson(id: Long)
+    suspend fun searchPerson(query: String): Flow<List<Person>>
+}

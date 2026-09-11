12853efb97e96709d205828f74f3814bfb3e814f
waxew <4622.ar@gmail.com>
2026-09-12 01:33:56 +0330

    Add interstitial ad manager layer for Tapsell

---
+++ app/src/main/java/com/asteam/itaghvim/core/ads/InterstitialAdManager.kt
@@ -0,0 +1,15 @@
+package com.asteam.itaghvim.core.ads
+
+/**
+ * مدیریت تبلیغ تمام صفحه تپسل
+ */
+class InterstitialAdManager {
+
+    fun isReady(): Boolean {
+        return false
+    }
+
+    fun showInterstitialAd() {
+        // اتصال SDK تپسل در این بخش انجام می شود.
+    }
+}

d3635113a981f074398b7b5cf1d03126ffeb8aa8
waxew <4622.ar@gmail.com>
2026-09-11 23:29:54 +0330

    Add backup manager foundation

---
+++ app/src/main/java/com/asteam/itaghvim/core/backup/BackupManager.kt
@@ -0,0 +1,18 @@
+package com.asteam.itaghvim.core.backup
+
+/**
+ * مدیریت اولیه پشتیبان گیری
+ * در نسخه های بعدی به Room Database و File Picker متصل می شود.
+ */
+class BackupManager {
+
+    // ایجاد فایل پشتیبان اطلاعات کاربر
+    fun backup() {
+        // پیاده سازی در مرحله اتصال دیتابیس
+    }
+
+    // بازیابی اطلاعات از فایل پشتیبان
+    fun restore() {
+        // پیاده سازی در مرحله اتصال File Picker
+    }
+}

8edb31d09e5d6b568deef9bf5c524a5498e90afb
waxew <4622.ar@gmail.com>
2026-09-12 01:50:36 +0330

    Add Premium DataStore constants

---
+++ app/src/main/java/com/asteam/itaghvim/core/ads/PremiumDataStoreConstants.kt
@@ -0,0 +1,9 @@
+package com.asteam.itaghvim.core.ads
+
+/**
+ * Central constants for Premium DataStore configuration.
+ */
+object PremiumDataStoreConstants {
+    const val FILE_NAME = "premium_preferences"
+    const val UNLOCKED_FEATURES_KEY = "unlocked_features"
+}

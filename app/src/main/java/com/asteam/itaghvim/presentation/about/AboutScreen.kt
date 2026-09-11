1136fa49ed1a6a2111b745a612927a202e4cf1a3
waxew <4622.ar@gmail.com>
2026-09-11 23:30:23 +0330

    Add about screen base implementation

---
+++ app/src/main/java/com/asteam/itaghvim/presentation/about/AboutScreen.kt
@@ -0,0 +1,12 @@
+package com.asteam.itaghvim.presentation.about
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+/**
+ * صفحه معرفی نرم افزار آی تقویم
+ */
+@Composable
+fun AboutScreen(){
+    Text(text = "آی تقویم\nمحصولی از AS Team")
+}

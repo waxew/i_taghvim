740ad9f1f07cfff22095ccddba9ec941bf64704d
waxew <4622.ar@gmail.com>
2026-09-12 00:34:15 +0330

    refactor: remove old asdev package AppModule

--- app/src/main/java/com/asdev/itaghvim/core/di/AppModule.kt
+++
@@ -1,9 +0,0 @@
-package com.asteam.itaghvim.core.di
-
-/**
- * ماژول عمومی Dependency Injection
- * محل قرارگیری وابستگی های عمومی برنامه
- */
-object AppModule {
-    // آماده برای اضافه شدن binding های عمومی
-}

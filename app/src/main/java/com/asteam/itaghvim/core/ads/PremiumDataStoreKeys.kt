f9ff74966505e8d402ab0bbec99d2038a2c0a75c
waxew <4622.ar@gmail.com>
2026-09-12 01:49:53 +0330

    Add Premium DataStore keys definition

---
+++ app/src/main/java/com/asteam/itaghvim/core/ads/PremiumDataStoreKeys.kt
@@ -0,0 +1,8 @@
+package com.asteam.itaghvim.core.ads
+
+/**
+ * Keys used for persistent Premium feature storage.
+ */
+object PremiumDataStoreKeys {
+    const val UNLOCKED_FEATURES = "unlocked_features"
+}

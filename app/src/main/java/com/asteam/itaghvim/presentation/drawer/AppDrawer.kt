59302536f4b3c767756114fdf57a9df2ee809ee1
waxew <4622.ar@gmail.com>
2026-09-11 23:28:19 +0330

    Add drawer ui foundation

---
+++ app/src/main/java/com/asteam/itaghvim/presentation/drawer/AppDrawer.kt
@@ -0,0 +1,13 @@
+package com.asteam.itaghvim.presentation.drawer
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+/**
+ * Drawer اصلی برنامه
+ * آیتم های منوی کناری در این بخش توسعه داده می شوند.
+ */
+@Composable
+fun AppDrawer() {
+    Text(text = "منوی آی تقویم")
+}

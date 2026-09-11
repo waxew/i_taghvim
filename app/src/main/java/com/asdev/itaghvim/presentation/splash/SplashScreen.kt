b2cafc9a5aa7ca1668f16cb851ba0a1805ceadb6
waxew <4622.ar@gmail.com>
2026-09-11 23:57:31 +0330

    Add splash screen foundation

---
+++ app/src/main/java/com/asdev/itaghvim/presentation/splash/SplashScreen.kt
@@ -0,0 +1,12 @@
+package com.asdev.itaghvim.presentation.splash
+
+import androidx.compose.runtime.Composable
+
+/**
+ * صفحه شروع برنامه آی تقویم
+ * در نسخه بعدی شامل Logo Motion خواهد شد.
+ */
+@Composable
+fun SplashScreen() {
+    // TODO: اضافه شدن انیمیشن لوگو
+}

634416128f40e0d6fa73dace9c7634e3316c93bd
waxew <4622.ar@gmail.com>
2026-09-12 01:12:29 +0330

    Complete EventViewModel Hilt integration

--- app/src/main/java/com/asteam/itaghvim/presentation/calendar/EventViewModel.kt
+++ app/src/main/java/com/asteam/itaghvim/presentation/calendar/EventViewModel.kt
@@ -2,15 +2,19 @@ package com.asteam.itaghvim.presentation.calendar
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import androidx.hilt.lifecycle.ViewModelInject
 import com.asteam.itaghvim.domain.model.CalendarEvent
 import com.asteam.itaghvim.domain.usecase.AddEventUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 /**
  * مدیریت منطق صفحه مناسبت ها
  * ارتباط UI با UseCase از طریق ViewModel انجام می شود.
  */
-class EventViewModel(
+@HiltViewModel
+class EventViewModel @Inject constructor(
     private val addEventUseCase: AddEventUseCase
 ) : ViewModel() {
 

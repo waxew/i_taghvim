f4273c04736a0ecf1ced86aede71e45629fb9ab2
waxew <4622.ar@gmail.com>
2026-09-11 23:26:39 +0330

    Add home UI state model

---
+++ app/src/main/java/com/asteam/itaghvim/presentation/home/HomeUiState.kt
@@ -0,0 +1,9 @@
+package com.asteam.itaghvim.presentation.home
+
+/**
+ * وضعیت صفحه اصلی برنامه
+ */
+data class HomeUiState(
+    val title: String = "آی تقویم",
+    val isLoading: Boolean = false
+)

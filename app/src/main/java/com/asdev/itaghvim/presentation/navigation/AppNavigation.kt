3989cba3a6e70653f5794f3e1627d6c9a5cd66d6
waxew <4622.ar@gmail.com>
2026-09-11 23:57:24 +0330

    Add navigation foundation

---
+++ app/src/main/java/com/asdev/itaghvim/presentation/navigation/AppNavigation.kt
@@ -0,0 +1,12 @@
+package com.asdev.itaghvim.presentation.navigation
+
+import androidx.compose.runtime.Composable
+
+/**
+ * مسیرهای اصلی ناوبری برنامه آی تقویم
+ * در نسخه های بعدی صفحات تقویم، اشخاص و تنظیمات اضافه می شوند.
+ */
+@Composable
+fun AppNavigation() {
+    // TODO: اتصال NavHost در مرحله بعد
+}

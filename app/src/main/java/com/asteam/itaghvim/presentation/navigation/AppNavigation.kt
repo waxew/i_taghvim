53aad7b8d62f12fcde7583a027069504542da43b
waxew <4622.ar@gmail.com>
2026-09-11 23:26:00 +0330

    Create presentation navigation layer

---
+++ app/src/main/java/com/asteam/itaghvim/presentation/navigation/AppNavigation.kt
@@ -0,0 +1,12 @@
+package com.asteam.itaghvim.presentation.navigation
+
+import androidx.compose.runtime.Composable
+
+/**
+ * Navigation مرکزی رابط کاربری
+ * صفحات در مراحل بعدی به NavHost اضافه می شوند.
+ */
+@Composable
+fun AppNavigation(){
+    // TODO ایجاد NavHost و مدیریت Back Stack
+}

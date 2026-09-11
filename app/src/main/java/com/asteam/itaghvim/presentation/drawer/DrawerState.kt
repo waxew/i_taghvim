a3412e8df5e5c0b14d98cb1c13dccea7061d6f14
waxew <4622.ar@gmail.com>
2026-09-11 23:30:16 +0330

    Add drawer state management model

---
+++ app/src/main/java/com/asteam/itaghvim/presentation/drawer/DrawerState.kt
@@ -0,0 +1,9 @@
+package com.asteam.itaghvim.presentation.drawer
+
+/**
+ * وضعیت منوی کناری برنامه آی تقویم
+ * برای کنترل باز و بسته شدن Drawer استفاده می شود.
+ */
+data class DrawerState(
+    val isOpen: Boolean = false
+)

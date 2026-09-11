e0a4e39063a43e2e8e3a2e40d929a1cbdf47fa47
waxew <4622.ar@gmail.com>
2026-09-11 23:29:14 +0330

    Add calendar viewmodel foundation

---
+++ app/src/main/java/com/asteam/itaghvim/presentation/calendar/CalendarViewModel.kt
@@ -0,0 +1,9 @@
+package com.asteam.itaghvim.presentation.calendar
+
+import androidx.lifecycle.ViewModel
+
+/**
+ * ViewModel صفحه تقویم
+ * منطق تغییر تاریخ و دریافت مناسبت ها در این بخش قرار می گیرد.
+ */
+class CalendarViewModel : ViewModel()

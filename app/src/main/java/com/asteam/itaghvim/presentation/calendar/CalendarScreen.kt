b9629a96832c21a4c3e05dbbc044d65a708337b3
waxew <4622.ar@gmail.com>
2026-09-11 23:28:46 +0330

    Add initial calendar screen

---
+++ app/src/main/java/com/asteam/itaghvim/presentation/calendar/CalendarScreen.kt
@@ -0,0 +1,13 @@
+package com.asteam.itaghvim.presentation.calendar
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+/**
+ * صفحه تقویم اصلی
+ * در نسخه های بعدی نمایش شمسی، قمری و میلادی اضافه خواهد شد.
+ */
+@Composable
+fun CalendarScreen() {
+    Text(text = "تقویم آی تقویم")
+}

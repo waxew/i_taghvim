f5a3b1a3fdf4a28a27baa4b3b4d0e380ce42e66d
waxew <4622.ar@gmail.com>
2026-09-12 00:32:58 +0330

    refactor: move AppModule to com.asteam.itaghvim package

---
+++ app/src/main/java/com/asteam/itaghvim/core/di/AppModule.kt
@@ -0,0 +1,9 @@
+package com.asteam.itaghvim.core.di
+
+/**
+ * ماژول عمومی Dependency Injection
+ * محل قرارگیری وابستگی های عمومی برنامه
+ */
+object AppModule {
+    // آماده برای اضافه شدن binding های عمومی
+}

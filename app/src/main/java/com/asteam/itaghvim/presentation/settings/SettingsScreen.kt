39372b68b281d0a35b4ca1b080fe869a0c294743
waxew <4622.ar@gmail.com>
2026-09-11 23:29:43 +0330

    Add initial settings screen

---
+++ app/src/main/java/com/asteam/itaghvim/presentation/settings/SettingsScreen.kt
@@ -0,0 +1,13 @@
+package com.asteam.itaghvim.presentation.settings
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+/**
+ * صفحه تنظیمات برنامه آی تقویم
+ * تنظیمات ظاهری، اعلان ها و مدیریت برنامه در این بخش توسعه داده می شود.
+ */
+@Composable
+fun SettingsScreen() {
+    Text(text = "تنظیمات آی تقویم")
+}

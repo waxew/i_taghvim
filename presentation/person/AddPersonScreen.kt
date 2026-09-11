c83cecd28fa0b1f4b5a22aded48a65c87e519aa0
waxew <4622.ar@gmail.com>
2026-09-11 23:44:20 +0330

    Create initial AddPerson screen

---
+++ presentation/person/AddPersonScreen.kt
@@ -0,0 +1,13 @@
+package com.asteam.itaghvim.presentation.person
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+// صفحه افزودن شخص
+// نسخه اولیه UI ایجاد شده است
+// اتصال به ViewModel و فرم کامل در مرحله بعد انجام می‌شود
+
+@Composable
+fun AddPersonScreen() {
+    Text(text = "افزودن شخص جدید")
+}

084faa33d46ab303ef806628871465a5e94f0761
waxew <4622.ar@gmail.com>
2026-09-12 01:31:10 +0330

    Add Tapsell configuration for iTaghvim ads

---
+++ app/src/main/java/com/asteam/itaghvim/core/ads/TapsellConfig.kt
@@ -0,0 +1,12 @@
+package com.asteam.itaghvim.core.ads
+
+/**
+ * تنظیمات تبلیغات تپسل آی تقویم
+ */
+object TapsellConfig {
+    const val APP_KEY = "jkicdkgdpbnsmnisqneqhgahqghdkrspjhtmhcaponjcepinktqmpfggmfmicppoqlppdt"
+
+    const val REWARDED_ZONE_ID = "6aa46a1c1f07c00619f450"
+
+    const val INTERSTITIAL_ZONE_ID = "6aa46a7a1f07c00619f450bb"
+}

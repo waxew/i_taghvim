dfe265ca94f8a3e78aa1736992678cafa7145b14
waxew <4622.ar@gmail.com>
2026-09-11 23:33:12 +0330

    Create add event screen foundation

---
+++ app/src/main/java/com/asteam/itaghvim/presentation/calendar/AddEventScreen.kt
@@ -0,0 +1,13 @@
+package com.asteam.itaghvim.presentation.calendar
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+/**
+ * صفحه افزودن مناسبت جدید
+ * فرم کامل در مراحل بعدی توسعه داده می شود.
+ */
+@Composable
+fun AddEventScreen() {
+    Text(text = "افزودن مناسبت جدید")
+}

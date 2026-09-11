97af7faba96d41d083b2d1d24795e663fac3c56b
waxew <4622.ar@gmail.com>
2026-09-12 01:33:08 +0330

    Add Tapsell banner configuration layer

---
+++ app/src/main/java/com/asteam/itaghvim/core/ads/BannerAdConfig.kt
@@ -0,0 +1,11 @@
+package com.asteam.itaghvim.core.ads
+
+/**
+ * تنظیمات جایگاه تبلیغ بنری تپسل
+ * مقدار Zone ID بعد از ساخت تبلیغ گاه Banner در پنل تپسل قرار می گیرد.
+ */
+object BannerAdConfig {
+    const val BANNER_ZONE_ID = ""
+
+    const val DEFAULT_POSITION = "BOTTOM"
+}

e7abda9b0164ddc1752f91d0be2860b24a0fc672
waxew <4622.ar@gmail.com>
2026-09-11 23:27:17 +0330

    Add initial home compose screen

---
+++ app/src/main/java/com/asteam/itaghvim/presentation/home/HomeScreen.kt
@@ -0,0 +1,13 @@
+package com.asteam.itaghvim.presentation.home
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+/**
+ * صفحه اصلی اولیه برنامه
+ * UI کامل در مراحل بعدی توسعه داده می شود.
+ */
+@Composable
+fun HomeScreen() {
+    Text(text = "آی تقویم")
+}

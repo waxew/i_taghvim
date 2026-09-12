f9626a12e6e29336152d36747ef375c042f69556
waxew <4622.ar@gmail.com>
2026-09-12 12:08:46 +0330

    Complete PersonViewModel CRUD use case wiring

--- presentation/person/PersonViewModel.kt
+++ presentation/person/PersonViewModel.kt
@@ -2,7 +2,11 @@ package com.asteam.itaghvim.presentation.person
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.asteam.itaghvim.domain.model.Person
+import com.asteam.itaghvim.domain.usecase.AddPersonUseCase
+import com.asteam.itaghvim.domain.usecase.DeletePersonUseCase
 import com.asteam.itaghvim.domain.usecase.GetPersonsUseCase
+import com.asteam.itaghvim.domain.usecase.UpdatePersonUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -11,23 +15,27 @@ import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
-// ViewModel مدیریت منطق صفحه اشخاص
 @HiltViewModel
 class PersonViewModel @Inject constructor(
-    private val getPersonsUseCase: GetPersonsUseCase
+    private val getPersonsUseCase: GetPersonsUseCase,
+    private val addPersonUseCase: AddPersonUseCase,
+    private val updatePersonUseCase: UpdatePersonUseCase,
+    private val deletePersonUseCase: DeletePersonUseCase
 ) : ViewModel() {
 
     private val _state = MutableStateFlow(PersonUiState())
     val state: StateFlow<PersonUiState> = _state
 
-    // دریافت لیست اشخاص از لایه Domain
     fun loadPersons() {
         viewModelScope.launch {
             _state.value = _state.value.copy(isLoading = true)
 
             getPersonsUseCase()
                 .catch {
-                    _state.value = _state.value.copy(isLoading = false)
+                    _state.value = _state.value.copy(
+                        isLoading = false,
+                        errorMessage = it.message
+                    )
                 }
                 .collectLatest { persons ->
                     _state.value = _state.value.copy(
@@ -35,6 +43,24 @@ class PersonViewModel @Inject constructor(
                         persons = persons
                     )
                 }
+        }
+    }
+
+    fun addPerson(person: Person) {
+        viewModelScope.launch {
+            addPersonUseCase(person)
+        }
+    }
+
+    fun updatePerson(person: Person) {
+        viewModelScope.launch {
+            updatePersonUseCase(person)
+        }
+    }
+
+    fun deletePerson(id: Long) {
+        viewModelScope.launch {
+            deletePersonUseCase(id)
         }
     }
 }

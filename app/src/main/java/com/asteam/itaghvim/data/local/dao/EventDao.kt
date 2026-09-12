c5fd6b9676ac39356125582d25301b72602caece
waxew <4622.ar@gmail.com>
2026-09-12 10:48:09 +0330

    Complete EventDao update delete and date query

--- app/src/main/java/com/asteam/itaghvim/data/local/dao/EventDao.kt
+++ app/src/main/java/com/asteam/itaghvim/data/local/dao/EventDao.kt
@@ -1,8 +1,10 @@
 package com.asteam.itaghvim.data.local.dao
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
+import androidx.room.Update
 import com.asteam.itaghvim.data.local.entity.EventEntity
 import kotlinx.coroutines.flow.Flow
 
@@ -15,8 +17,17 @@ interface EventDao {
     @Insert
     suspend fun insert(event: EventEntity)
 
+    @Update
+    suspend fun update(event: EventEntity)
+
+    @Delete
+    suspend fun delete(event: EventEntity)
+
     @Query("SELECT * FROM events ORDER BY dateSolar")
     fun getAll(): Flow<List<EventEntity>>
+
+    @Query("SELECT * FROM events WHERE dateSolar = :date")
+    fun getEventsByDate(date: String): Flow<List<EventEntity>>
 
     @Query("SELECT * FROM events WHERE personName LIKE '%' || :name || '%'")
     fun search(name: String): Flow<List<EventEntity>>

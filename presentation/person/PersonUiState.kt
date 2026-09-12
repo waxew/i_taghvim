d5210f5063814504593a8aefafd936aae2456f15
waxew <4622.ar@gmail.com>
2026-09-12 12:06:57 +0330

    Fix PersonUiState to use domain Person model

--- presentation/person/PersonUiState.kt
+++ presentation/person/PersonUiState.kt
@@ -1,8 +1,10 @@
 package com.asteam.itaghvim.presentation.person
 
+import com.asteam.itaghvim.domain.model.Person
+
 // وضعیت صفحه مدیریت اشخاص برای UI
- data class PersonUiState(
+data class PersonUiState(
     val isLoading: Boolean = false,
-    val persons: List<String> = emptyList(),
+    val persons: List<Person> = emptyList(),
     val errorMessage: String? = null
 )

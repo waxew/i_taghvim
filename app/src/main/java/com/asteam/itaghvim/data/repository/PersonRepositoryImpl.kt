fb2bf02f47e3096d929b7c6ced81422b2ee64554
waxew <4622.ar@gmail.com>
2026-09-12 11:57:12 +0330

    Complete PersonRepositoryImpl Room integration

--- app/src/main/java/com/asteam/itaghvim/data/repository/PersonRepositoryImpl.kt
+++ app/src/main/java/com/asteam/itaghvim/data/repository/PersonRepositoryImpl.kt
@@ -1,22 +1,66 @@
 package com.asteam.itaghvim.data.repository
 
+import com.asteam.itaghvim.data.local.dao.PersonDao
+import com.asteam.itaghvim.data.local.entity.PersonEntity
+import com.asteam.itaghvim.domain.model.Person
 import com.asteam.itaghvim.domain.repository.PersonRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
 
 /**
- * پیاده سازی Repository
- * ارتباط بین Domain و Data در این لایه انجام می شود.
+ * ارتباط بین Domain و Data
+ * پیاده سازی CRUD اشخاص با Room
  */
-class PersonRepositoryImpl : PersonRepository {
+class PersonRepositoryImpl @Inject constructor(
+    private val personDao: PersonDao
+) : PersonRepository {
 
-    override fun getPersons() = TODO("در مرحله اتصال Room تکمیل می شود")
+    override fun getPersons(): Flow<List<Person>> {
+        return personDao.getAll().map { persons ->
+            persons.map { it.toDomain() }
+        }
+    }
 
-    override suspend fun insertPerson(person: com.asteam.itaghvim.domain.model.Person) {
-        // TODO اتصال به DAO
+    override suspend fun insertPerson(person: Person) {
+        personDao.insert(person.toEntity())
     }
 
     override suspend fun deletePerson(id: Long) {
-        // TODO اتصال به DAO
+        personDao.delete(
+            PersonEntity(
+                id = id,
+                name = "",
+                birthday = null,
+                marriageDate = null,
+                notes = null
+            )
+        )
     }
 
-    override suspend fun searchPerson(query: String) = TODO("جستجو از DAO")
+    override fun searchPerson(query: String): Flow<List<Person>> {
+        return personDao.search(query).map { persons ->
+            persons.map { it.toDomain() }
+        }
+    }
+
+    private fun PersonEntity.toDomain(): Person {
+        return Person(
+            id = id,
+            name = name,
+            birthday = birthday,
+            marriageDate = marriageDate,
+            notes = notes
+        )
+    }
+
+    private fun Person.toEntity(): PersonEntity {
+        return PersonEntity(
+            id = id,
+            name = name,
+            birthday = birthday,
+            marriageDate = marriageDate,
+            notes = notes
+        )
+    }
 }

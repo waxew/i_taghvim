8e49f99d9cc5f5a18f02c0b992f8145243dc43d5
waxew <4622.ar@gmail.com>
2026-09-12 11:41:28 +0330

    Align delete event use case with repository domain model

--- app/src/main/java/com/asteam/itaghvim/domain/usecase/DeleteEventUseCase.kt
+++ app/src/main/java/com/asteam/itaghvim/domain/usecase/DeleteEventUseCase.kt
@@ -1,14 +1,16 @@
 package com.asteam.itaghvim.domain.usecase
 
+import com.asteam.itaghvim.domain.model.CalendarEvent
 import com.asteam.itaghvim.domain.repository.EventRepository
+import javax.inject.Inject
 
 /**
  * حذف مناسبت از برنامه
  */
-class DeleteEventUseCase(
+class DeleteEventUseCase @Inject constructor(
     private val repository: EventRepository
 ) {
-    suspend operator fun invoke(id: Long) {
-        repository.deleteEvent(id)
+    suspend operator fun invoke(event: CalendarEvent) {
+        repository.deleteEvent(event)
     }
 }
